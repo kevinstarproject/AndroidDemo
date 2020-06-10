5b880271e6d79c27153c364880d38ba2c17f2d61
kevinstarproject <65999867+kevinstarproject@users.noreply.github.com>
2020-06-10 10:04:17 +0800

    Modifying MainViewModel for fetching user data.

--- app/src/main/java/com/demo/githubuserlist/ui/main/MainViewModel.kt
+++ app/src/main/java/com/demo/githubuserlist/ui/main/MainViewModel.kt
@@ -1,7 +1,43 @@
 package com.demo.githubuserlist.ui.main
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.demo.githubuserlist.data.api.repository.MainRepository
+import com.demo.githubuserlist.data.model.User
+import com.demo.githubuserlist.utils.Resource
+import io.reactivex.android.schedulers.AndroidSchedulers
+import io.reactivex.disposables.CompositeDisposable
+import io.reactivex.schedulers.Schedulers
 
-class MainViewModel : ViewModel() {
-    // TODO: Implement the ViewModel
+class MainViewModel(private val mainRepository: MainRepository) : ViewModel() {
+    private val users = MutableLiveData<Resource<List<User>>>()
+    private val compositeDisposable = CompositeDisposable()
+    init {
+        fetchUsers()
+    }
+
+    private fun fetchUsers() {
+        users.postValue(Resource.loading(null))
+        compositeDisposable.add(
+                mainRepository.getUsers()
+                        .subscribeOn(Schedulers.io())
+                        .observeOn(AndroidSchedulers.mainThread())
+                        .subscribe({ userList ->
+                            users.postValue(Resource.success(userList))
+                        }, { throwable ->
+                            users.postValue(Resource.error("Error", null))
+                        })
+        )
+    }
+
+    override fun onCleared() {
+        super.onCleared()
+        compositeDisposable.dispose()
+    }
+
+    fun getUsers(): LiveData<Resource<List<User>>> {
+        return users
+    }
+
 }

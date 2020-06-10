78f052f01ab4b04a06b827547712b9b24b56ef63
kevinstarproject <65999867+kevinstarproject@users.noreply.github.com>
2020-06-10 10:03:35 +0800

    Initializing fetching function.

--- app/src/main/java/com/demo/githubuserlist/MainActivity.kt
+++ app/src/main/java/com/demo/githubuserlist/MainActivity.kt
@@ -2,12 +2,14 @@ package com.demo.githubuserlist
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.androidnetworking.AndroidNetworking
 import com.demo.githubuserlist.ui.main.MainFragment
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        AndroidNetworking.initialize(getApplicationContext());
         setContentView(R.layout.main_activity)
         if (savedInstanceState == null) {
             supportFragmentManager.beginTransaction()
